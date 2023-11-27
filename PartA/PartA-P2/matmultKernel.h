0a51214b2f44d9d0c66812315b2ea255f8c13e6b
Jinli Duan <jinlid@uci.edu>
2023-11-26 23:06:10 -0500

    Update matmultKernel.h

--- PartA/PartA-P2/matmultKernel.h
+++ PartA/PartA-P2/matmultKernel.h
@@ -2,19 +2,19 @@
 // For ECE-GY 9143 - High Performance Computing for Machine Learning
 // Instructor: Parijat Dubey
 
-// Kernels defined with this header must 
+// Kernels defined with this header must
 // multiply two matrices using CUDA: A x B = C
 
 #ifndef __MMKERNEL__
 #define __MMKERNEL__
 
 // Defines for size of thread block and data computed by a thread block
-#define BLOCK_SIZE 16
+#define BLOCK_SIZE 32
 #ifndef FOOTPRINT_SIZE
 #define FOOTPRINT_SIZE BLOCK_SIZE
 #endif
 
-// The type Matrix is really a MATRIX DESCRIPTOR. 
+// The type Matrix is really a MATRIX DESCRIPTOR.
 // Matrices are stored in row major order:
 //       M[row,col] = *(M.elements + row * M.stride + col)
 //
@@ -23,7 +23,7 @@
 // This requires the stride of the full matrix to properly get to the
 // next row of the sub matrix (a block).
 //
-// Stride is the width in bytes from one element of the larger matrix 
+// Stride is the width in bytes from one element of the larger matrix
 // to the element in the same column but one row down.
 
 
@@ -38,4 +38,3 @@ typedef struct {
 __global__ void MatMulKernel(const Matrix, const Matrix, Matrix);
 
 #endif
-
